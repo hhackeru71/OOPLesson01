1d60c470556cea7909e963563ddc419e7497e15c
spaceshipwalls@gmail.com <spaceshipwalls@gmail.com>
2022-10-20 19:39:00 +0300

    Add project files.

---
+++ OOPLesson1/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPLesson1
+{
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+
+        public void Print()
+        {
+            Console.WriteLine($"{firstName}{lastName}{age}");
+        }
+
+
+
+        //פונקציה  שמחזירה את הגיל של המשתמש 
+
+        //פונקציה שמקבלת את השם של המשתמש   ובודקת האם המחרוזת האורך שלה יותר מ5 
+        //במידה וכן מחזירה true
+        //אחרת false
+
+
+    }
+}
+
---
+++ OOPLesson1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OOPLesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person ps = new Person();
+            ps.firstName = "yarin";
+            ps.lastName = "sh";
+            ps.age = 50;
+            ps.Print();
+            //שמדפיס את הגיל של המשתמש ע"י הפונקציה שמחזירה את הגיל של המשתמש
+            //להדפיס האם האורך של המחרוזת   יותר מ5 ע"י שימוש בפונקציה שבודקת
+
+        }
+    }
+}
